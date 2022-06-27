147dedaf054f861e42a3197adb82215a082b1e46
SantoshKumar353 <96779640+SantoshKumar353@users.noreply.github.com>
2022-06-27 21:26:50 +0530

    a, b = map(int, input().split()) while a!=b:     if a>b:         a=a-b     elif a<b:         b=b-a print(a)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,11 +1,7 @@
-def hcf_gcd(a,b):
+a, b = map(int, input().split())
+while a!=b:
     if a>b:
-        max=a
-    else:
-        max=b
-    while True:
-        if a%max==0 and b%max==0:
-            return max
-        max-=1
-a,b=map(int,input().split())
-print(hcf_gcd(a,b))
+        a=a-b
+    elif a<b:
+        b=b-a
+print(a)
