c7158cd9a5e2080ce50c19a106d9770a7eed9a52
SantoshKumar353 <96779640+SantoshKumar353@users.noreply.github.com>
2022-06-27 21:36:58 +0530

    n=int(input()) length=len(str(n)) temp=n i=0 s=0 while temp>0:     i=temp%10     s=s+int(i**length)     temp=temp//10     length=length-1 if s==n:     print(True) else:     print(False)

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,16 +1,14 @@
-o=int(input())
-temp=o
-d=0
-while temp:
-    d+=1
-    temp//=10
-number=o
-sum=0
-while o:
-    sum+=(o%10)**d
-    o//=10
-    d-=1
-if sum==number:
+n=int(input())
+length=len(str(n))
+temp=n
+i=0
+s=0
+while temp>0:
+    i=temp%10
+    s=s+int(i**length)
+    temp=temp//10
+    length=length-1
+if s==n:
     print(True)
 else:
     print(False)
