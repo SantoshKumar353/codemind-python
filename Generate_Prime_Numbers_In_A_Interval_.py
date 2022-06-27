f68a1ea6245bcda9ce147340076fd51150051d3d
SantoshKumar353 <96779640+SantoshKumar353@users.noreply.github.com>
2022-06-27 21:08:50 +0530

    a = int(input()) b = int(input()) while a<=b:     c = 0     x = 1     while x<=a//2:         if a%x==0:             c+=1         x+=1     if c==1:         print(a)     a+=1

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,12 +1,12 @@
-g=int(input())
-h=int(input())
-while g<=h:
-    c=0
-    x=1
-    while x<=g//2:
-        if g%x==0:
+a = int(input())
+b = int(input())
+while a<=b:
+    c = 0
+    x = 1
+    while x<=a//2:
+        if a%x==0:
             c+=1
         x+=1
     if c==1:
-        print(g)
-    g+=1
+        print(a)
+    a+=1
