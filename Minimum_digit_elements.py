cddfd4e3b0b19a6d58078bebfe3aa20f8e46fd3e
SantoshKumar353 <96779640+SantoshKumar353@users.noreply.github.com>
2022-07-02 22:06:18 +0530

    n=int(input()) a=list(map(int,input().split())) m=min(a) c=0 for i in a:     if len(str(m))==len(str(i)):         c+=1 print(c)

--- Minimum_digit_elements.py
+++ Minimum_digit_elements.py
@@ -1,11 +1,8 @@
-def min_dig(k):
-    a=list(map(int,input().split()))
-    m=min(a)
-    c=0
-    for i in a:
-        if len(str(m))==len(str(i)):
-            c+=1
-    print(c)
-    
-k=int(input())
-min_dig(k)
+n=int(input())
+a=list(map(int,input().split()))
+m=min(a)
+c=0
+for i in a:
+    if len(str(m))==len(str(i)):
+        c+=1
+print(c)
