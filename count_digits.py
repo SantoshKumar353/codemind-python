c9dc4cd8fae4b68c8cd62e7e26c7d35edc166f74
SantoshKumar353 <96779640+SantoshKumar353@users.noreply.github.com>
2022-07-02 22:14:03 +0530

    n=int(input()) l=list(map(int,input().split())) N=[] for i in l:     N.append(len(str(abs(i)))) print(*N)

--- count_digits.py
+++ count_digits.py
@@ -1,9 +1,6 @@
-def count(k):
-    l=list(map(int,input().split()))
-    N=[]
-    for i in l:
-        N.append(len(str(abs(i))))
-    print(*N)
-    
-k=int(input())
-count(k)
+n=int(input())
+l=list(map(int,input().split()))
+N=[]
+for i in l:
+    N.append(len(str(abs(i))))
+print(*N)
